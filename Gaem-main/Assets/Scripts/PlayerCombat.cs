0c0ce54c1c61148a8348b3f47a7d663b85d8be31
SiPrego <granberg.fhilip@gmail.com>
2021-01-26 10:50:07 +0100

    merge?

--- Gaem-main/Assets/Scripts/PlayerCombat.cs
+++ Gaem-main/Assets/Scripts/PlayerCombat.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    //public Animator animator;
+   
 
-    public Transform attack_point;
-    public float attackRange = 0.5f;
-    public LayerMask enemyleyers;
     
     void Update()
     {
@@ -18,57 +15,8 @@ public class PlayerCombat : MonoBehaviour
         }
     }
 
-    //vadsom händer när "attack" sker
     void Attack()
     {
-        //animation
-        //animator.SetTrigger("Attack");
-
-        // kod för att "märka" vilka enemy som är in range och kan bli träffade
-        Collider[] hitenemys = Physics.OverlapSphere(attack_point.position, attackRange, enemyleyers);  
-
-        //skada
-
-        foreach(Collider enemy in hitenemys)
-        {
-           // enemy.GetComponent<enemy>().TakeDamge;
-
-
-            Debug.Log("we Hit " + enemy.name);
-
-
-        }
-
 
     }
-
-
-    void OnDrawGizmosSelected()
-    {
-
-        if (attack_point == null)
-            return;
-        Gizmos.DrawWireSphere(attack_point.position, attackRange);
-        
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
---
+++ Gaem-main/Gaem-main/Assets/enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class enemy : MonoBehaviour
+
+{
+    public int maxhp==100;
+
+    private int currenthp;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+
+        currenthp = maxhp;
+
+    }
+
+    public void takedmg(int damge)
+    {
+
+        currenthp -= damge;
+
+        if (currenthp <= 0)
+        {
+            Die();
+
+        }
+
+
+    }
+
+    
+}
+*/
